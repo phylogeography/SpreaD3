38cf58de0fbd21cd825d2ce145f8823aa18ac63c
fbielejec <fbielejec@gmail.com>
2015-04-20 15:30:59 +0100

    testing JSON representations

--- src/test/Test.java
+++ src/test/Test.java
@@ -1,16 +1,42 @@
 package test;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.fasterxml.jackson.core.JsonGenerationException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 
 public class Test {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws JsonMappingException, JsonGenerationException {
 		
-		// try JSON databind: https://github.com/FasterXML/jackson-databind/wiki
-		// http://wiki.fasterxml.com/JacksonInFiveMinutes#A.22Raw.22_Data_Binding_Example
+		 try {
+		
 		  ObjectMapper mapper = new ObjectMapper();
-
+		  
+		  Map<String,Object> userData = new HashMap<String,Object>();
+		  Map<String,String> nameStruct = new HashMap<String,String>();
+		  
+		  nameStruct.put("first", "Joe");
+		  nameStruct.put("last", "Sixpack");
+		  
+		  userData.put("name", nameStruct);
+		  userData.put("gender", "MALE");
+		  userData.put("verified", Boolean.FALSE);
+		  
+		  
+			mapper.writeValue(new File("test.json"), userData);
+			
+			System.out.println("Finished");
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		  
 	}//END: main
 
 }//END: class
