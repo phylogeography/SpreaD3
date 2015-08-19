cd78a79ffe31c0f851f6b6a1486fd093a32ccee0
filip <fbielejec@gmail.com>
2015-08-19 16:05:33 +0200

    working on rolling map

--- d3renderer/src/script.js
+++ d3renderer/src/script.js
@@ -2,26 +2,48 @@
  * @fbielejec
  */
 
-// ////////////////////////
-// ---GLOBAL VARIABLES---//
-// ////////////////////////
+// ///////////////////
+// --- VARIABLES ---//
+// ///////////////////
+
+//---MAP---//
+
 var width = document.getElementById('container').offsetWidth;
 var height = width / 2;
+var rotate = 60;
+var maxlat = 83;
 
-var sliceCount = 5;
+var projection = d3.geo.mercator() //
+.rotate([ rotate, 0 ]) //
+.scale(1) //
+.translate([ width / 2, height / 2 ]);
 
-var topo;
-var projection;
+// set up the scale extent and initial scale for the projection
+var b = mercatorBounds(projection, maxlat);
+var s = width / (b[1][0] - b[0][0]);
+var scaleExtent = [ s, 10 * s ];
+
+var tlast = [ 0, 0 ];
+var slast = null;
+
+var zoom = d3.behavior.zoom() //
+.scaleExtent(scaleExtent) //
+//.scale(projection.scale()) //
+//.translate([ 0, 0 ]) //
+.on("zoom", redraw);
+
+//var topo;
 var path;
 var svg;
 var g;
 
-var throttleTimer;
-
 var graticule = d3.geo.graticule();
 
-var tooltip = d3.select("#container").append("div").attr("class",
-		"tooltip hidden");
+var throttleTimer;
+
+//---DATA---//
+
+var sliceCount = 5;
 
 var locations;
 
@@ -44,34 +66,92 @@ var lineColorSelect;
 // ---FUNCTIONS---//
 // /////////////////
 
-// ---SETUP---//
+function mercatorBounds(projection, maxlat) {
+
+	var yaw = projection.rotate()[0];
+	var xymax = projection([ -yaw + 180 - 1e-6, -maxlat ]);
+	var xymin = projection([ -yaw - 180 + 1e-6, maxlat ]);
+
+	return [ xymin, xymax ];
+}// END: mercatorBounds
+
+function click() {
+	projection.invert(d3.mouse(this));
+}// END: click
+
+function throttle() {
+
+	window.clearTimeout(throttleTimer);
+	throttleTimer = window.setTimeout(function() {
+		redraw();
+	}, 200);
+
+}// END: throttle
+
 function setup(width, height) {
 
-	projection = d3.geo.mercator() //
-	.translate([ (width / 2), (height / 2) ]) //
-	.scale(width / 2 / Math.PI);
+	 path = d3.geo.path().projection(projection);
 
-	path = d3.geo.path().projection(projection);
-
-	svg = d3.select("#container").append("svg") //
+	 svg = d3.select("#container").append("svg") //
 	.attr("width", width) //
 	.attr("height", height) //
 	.call(zoom) //
-	.on("click", click);
+	 .on("click", click);
 
-	g = svg.append("g");
+	 g = svg.append("g");
 
 }// END: setup
 
-// ---DRAW---//
+function redraw(topo) {
 
-function draw(topo) {
+	if (d3.event) {
 
+		var scale = d3.event.scale;
+		var t = d3.event.translate;
+
+		if (scale != slast) {
+
+			projection.scale(scale);
+
+		} else {
+
+			var dx = t[0] - tlast[0], dy = t[1] - tlast[1];
+			var yaw = projection.rotate()[0];
+			var tp = projection.translate();
+
+			// use x translation to rotate based on current scale
+			projection.rotate([
+					yaw + 360. * dx / width * scaleExtent[0] / scale, 0, 0 ]);
+			// use y translation to translate projection, clamped by min/max
+			var b = mercatorBounds(projection, maxlat);
+
+			if (b[0][1] + dy > 0) {
+
+				dy = -b[0][1];
+
+			} else if (b[1][1] + dy < height) {
+
+				dy = height - b[1][1];
+
+			}
+
+			projection.translate([ tp[0], tp[1] + dy ]);
+		}
+
+		// save last values. resetting zoom.translate() and scale() would
+		// seem equivalent but doesn't seem to work reliably?
+		slast = scale;
+		tlast = t;
+
+	}
+
+	// grid of longitude and latitude lines
 	svg.append("path") //
 	.datum(graticule) //
 	.attr("class", "graticule") //
 	.attr("d", path);
 
+	// equator
 	g.append("path") //
 	.datum(
 			{
@@ -82,139 +162,60 @@ function draw(topo) {
 	.attr("class", "equator") //
 	.attr("d", path);
 
+	// re-project path data
 	var country = g.selectAll(".country").data(topo);
 
 	country.enter().insert("path") //
 	.attr("class", "country") //
 	.attr("d", path) //
-	.attr("id", function(d, i) {
-		return d.id;
-	}) //
-	.attr("title", function(d, i) {
-		return d.properties.name;
-	}) //
-	.style("fill", "rgb(194, 178, 128)") //
-	.style("stroke", "rgb(0, 0, 0)") //
-	;
-
-	// offsets for tooltips
-	var offsetL = document.getElementById('container').offsetLeft + 20;
-	var offsetT = document.getElementById('container').offsetTop + 10;
-
-	// tooltips
-	country.on(
-			"mouseover",
-			function(d, i) {
-
-				var mouse = d3.mouse(svg.node()).map(function(d) {
-					return parseInt(d);
-				});
-
-				tooltip.classed("hidden", false) //
-				.attr(
-						"style",
-						"left:" + (mouse[0] + offsetL) + "px;top:"
-								+ (mouse[1] + offsetT) + "px") //
-				.html(d.properties.name);
-
-				d3.select(this).style("fill", "rgb(250, 250, 250)");
-
-			}) //
-	.on("mouseout", function(d, i) {
-
-		tooltip.classed("hidden", true);
-
-		d3.select(this).style("fill", "rgb(194, 178, 128)");
-
-	});
-
-}// END: draw
-
-// ---REDRAW---//
-
-function redraw() {
-
-	width = document.getElementById('container').offsetWidth;
-	height = width / 2;
-	d3.select('svg').remove();
-	setup(width, height);
-	draw(topo);
-
-}// END: redraw
-
-// ---MOVE---//
-
-function move() {
-
-	var t = d3.event.translate;
-	var s = d3.event.scale;
-	// zscale = s;
-	var h = height / 4;
-
-	t[0] = Math.min((width / height) * (s - 1), //
-	Math.max(width * (1 - s), t[0]) //
-	);
-
-	t[1] = Math.min(h * (s - 1) + h * s, //
-	Math.max(height * (1 - s) - h * s, t[1]) //
-	);
-
-	zoom.translate(t);
-	g.attr("transform", "translate(" + t + ")scale(" + s + ")");
-
+	 .style("fill", "rgb(194, 178, 128)") //
+	 .style("stroke", "rgb(0, 0, 0)")
+	 ;
+	
 	// adjust the country hover stroke width based on zoom level
 	d3.selectAll(".country") //
-	.style("stroke-width", 1.5 / s) //
-	;
+	.style("stroke-width", 1.5 / slast) ;
+	
+	// re-project path data
+//	 svg.selectAll('path') //
+//	 .attr("class", "country") //
+//	 .attr('d', path) //
+//	 .style("fill", "rgb(194, 178, 128)") //
+//	 .style("stroke", "rgb(0, 0, 0)");
 
-}// END: move
-
-// ---THROTTLE---//
-
-function throttle() {
-
-	window.clearTimeout(throttleTimer);
-
-	throttleTimer = window.setTimeout(function() {
-		redraw();
-	}, 200);
-
-}// END: throttle
-
-// ---CLICK---//
-
-function click() {
-	/* Geo translation on mouse click on map */
-	projection.invert(d3.mouse(this));
-}// END: click
+}// END: redraw
 
 // /////////////////
 // ---RENDERING---//
 // /////////////////
 
-// ---MAP BACKGROUND---//
+// ---DRAW MAP BACKGROUND---//
 
 d3.select(window).on("resize", throttle);
 
-var zoom = d3.behavior.zoom() //
-.scaleExtent([ 1, 9 ]) //
-.on("zoom", move);
+projection.scale(scaleExtent[0]);
 
 setup(width, height);
 
 d3.json("data/world-topo-min.json", function(error, world) {
 
-	topo = topojson.feature(world, world.objects.countries).features;
-	draw(topo);
+	 topo = topojson.feature(world, world.objects.countries).features;
+
+//	 svg.selectAll('path') //
+//	 .data(topo) //
+//	 .enter() //
+//	 .append('path');
+
+	redraw(topo);
 
 });
 
-// ---DRAW DATA---//
+//---DRAW DATA---//
 
 d3.json("data/test_discrete.json", function(json) {
 
 	// paint all, then manipulate visibility according to dates
-	
+
 	var timeLine = json.timeLine;
 	var startDate = new Date(timeLine.startTime);
 	var endDate = new Date(timeLine.endTime);
@@ -245,15 +246,18 @@ d3.json("data/test_discrete.json", function(json) {
 		populatePolygonMaps(polygons);
 		generatePolygons(polygons, locations, locationIds);
 
-		 lines = layer.lines;
-		 populateLineMaps(lines);
-//		 generateLines(lines, locations, locationIds);
+		lines = layer.lines;
+		populateLineMaps(lines);
+		generateLines(lines, locations, locationIds);
 
 	});
 
 	// polygon area listener
 	d3.select(polygonAreaSelect).on('change', function() {
-		//TODO: listeners should also 'rewind time', or set the visibility to current setting on the slider
+		
+		// TODO: listeners should also 'rewind time', or set the visibility to
+		// current setting on the slider
+		
 		g.selectAll(".polygon").remove();
 		generatePolygons(polygons, locations, locationIds);
 
@@ -268,13 +272,13 @@ d3.json("data/test_discrete.json", function(json) {
 	});
 
 	// line color listener
-	 d3.select(lineColorSelect).on('change', function() {
-	
-	 g.selectAll(".line").remove();
-	 generateLines(lines, locations, locationIds);
-	
-	 });
-	
+	d3.select(lineColorSelect).on('change', function() {
+
+		g.selectAll(".line").remove();
+		generateLines(lines, locations, locationIds);
+
+	});
+
 	// time slider listener
 	timeSlider.on('slide', function(evt, value) {
 
@@ -282,62 +286,51 @@ d3.json("data/test_discrete.json", function(json) {
 		currentDateDisplay.text(dateFormat(currentDate));
 
 		// set transparency (opacity) on elements up to current date
-		
+
 		// polygons
 		d3.selectAll(".polygon")[0]
-		.filter(function(polygon) {
+				.filter(function(polygon) {
 
-			var polygonStartDate = new Date(polygon.attributes.startTime.value);
-			if (polygonStartDate <= value) {
+					var polygonStartDate = new Date(
+							polygon.attributes.startTime.value);
+					if (polygonStartDate <= value) {
 
-				// TODO: transition
-				
-//				polygon.setAttribute('opacity', 1);
-				
-				
-					
-					d3.select(polygon) //
-					.transition() //
-			    .duration(750)//
-			    .attr("opacity", 1);
-				
-//				console.log(tmp);
-//				tmp.transition()
-//			    .duration(750)
-//			    .style("fill", "steelblue");
-				
-			} else {
-				
-				polygon.setAttribute('opacity', 0);
-				
-			}//END: date check
+						d3.select(polygon) //
+						.transition() //
+						.duration(750) //
+						.attr("opacity", 1);
 
-		})
-		;//END: filter
+					} else {
 
-		
+						d3.select(polygon).attr("opacity", 0);
+						
+//						polygon.setAttribute('opacity', 0);
+
+					}// END: date check
+
+				});// END: filter
+
 		// lines
 		d3.selectAll(".line")[0].filter(function(line) {
-		
+
 			var lineStartDate = new Date(line.attributes.startTime.value);
 			if (lineStartDate <= value) {
 
-				line.setAttribute('opacity', 1);
-				
+				d3.select(line) //
+				.transition() //
+				.duration(750) //
+				.attr("opacity", 1);
+
 			} else {
+
+				d3.select(line).attr("opacity", 0);
 				
-				line.setAttribute('opacity', 0);
-				
-			}//END: date check
-			
-			
-			
-		});//END: filter
-		
-		
-		
-	});//END: slide
+//				line.setAttribute('opacity', 0);
+
+			}// END: date check
+
+		});// END: filter
+
+	});// END: slide
 
 });
-
-
