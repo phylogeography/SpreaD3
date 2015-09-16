fbfd4bb44c215a24cc67a3ef4ab36e7e6047eeb2
filip <fbielejec@gmail.com>
2015-09-16 15:22:02 +0200

    slider and timing for animations

--- d3renderer/src/script.js
+++ d3renderer/src/script.js
@@ -67,83 +67,83 @@ function initializeLayers(layers) {
 
 }// END: initializeLayers
 
+
+function paintFrame(value, timeScale, currentDateDisplay,
+		dateFormat) {
+	
+	var currentDate = timeScale.invert(timeScale(value));
+	currentDateDisplay.text(dateFormat(currentDate));
+
+	d3.selectAll(".line")[0]
+			.forEach(function(line) {
+
+				var linePath = d3.select(line)
+				var totalLength = linePath.node().getTotalLength();
+
+				var lineStartDate = new Date(
+						linePath.node().attributes.startTime.value);
+				var lineEndDate = new Date(
+						linePath.node().attributes.endTime.value);
+
+				if (lineStartDate <= value && value <= lineEndDate) {// painting
+
+					var duration = lineEndDate - lineStartDate;
+					var timePassed = value - lineStartDate;
+
+					var offset = map(timePassed, 0, duration, 0,
+							totalLength);
+					offset = totalLength - offset;
+
+					linePath //
+					.transition() //
+					.duration(750) //
+					.ease("linear") //
+					.attr("stroke-dashoffset", offset) //
+					.attr("opacity", 1);
+
+				} else if (lineStartDate > value) { // not yet
+
+					linePath.attr("stroke-dasharray",
+							totalLength + " " + totalLength) //
+					.attr("stroke-dashoffset", totalLength) //
+					.attr("opacity", 0);
+
+				} else if (lineEndDate < value) { // already painted
+
+					linePath.attr("stroke-dasharray",
+							totalLength + " " + totalLength) //
+					.attr("stroke-dashoffset", 0) //
+					.attr("opacity", 1);
+
+				} else { // sth went wrong
+
+					console.log("FUBAR");
+
+				}// END: time check
+
+			});// END: filter
+	
+}//END: paintFrame
+
+
+
 function initializeTimeSlider(timeSlider, timeScale, currentDateDisplay,
 		dateFormat) {
 
 	// time slider listener
 	timeSlider.on('slide', function(evt, value) {
 
-		// console.log(value);
+		paintFrame(value, timeScale, currentDateDisplay,
+				dateFormat);
 
-		var currentDate = timeScale.invert(timeScale(value));
-		currentDateDisplay.text(dateFormat(currentDate));
-
-		d3.selectAll(".line")[0]
-				.forEach(function(line) {
-
-					var linePath = d3.select(line)
-					var totalLength = linePath.node().getTotalLength();
-
-					var lineStartDate = new Date(
-							linePath.node().attributes.startTime.value);
-					var lineEndDate = new Date(
-							linePath.node().attributes.endTime.value);
-
-					if (lineStartDate <= value && value <= lineEndDate) {// painting
-
-						var duration = lineEndDate - lineStartDate;
-						var timePassed = value - lineStartDate;
-
-						var offset = map(timePassed, 0, duration, 0,
-								totalLength);
-						offset = totalLength - offset;
-
-						linePath //
-						.transition() //
-						.duration(750) //
-						.ease("linear") //
-						.attr("stroke-dashoffset", offset) //
-						.attr("opacity", 1);
-
-					} else if (lineStartDate > value) { // not yet
-
-						linePath.attr("stroke-dasharray",
-								totalLength + " " + totalLength) //
-						.attr("stroke-dashoffset", totalLength) //
-						.attr("opacity", 0);
-
-					} else if (lineEndDate < value) { // already painted
-
-						linePath.attr("stroke-dasharray",
-								totalLength + " " + totalLength) //
-						.attr("stroke-dashoffset", 0) //
-						.attr("opacity", 1);
-
-					} else { // sth went wrong
-
-						console.log("FUBAR");
-
-					}// END: time check
-
-				});// END: filter
+		currentSliderValue = value;
+	
 
 	});// END: slide
 
 }// END: initializeTimeSlider
 
-// TODO: not registered with listener
-var interval = 86400000;
-var tick = 1;
-function testTimer() {
 
-	var sliderValue = sliderStartValue + tick * interval;
-
-	timeSlider.value(sliderValue);
-	tick++;
-
-	// console.log(sliderValue);
-
-}
 
 // /////////////////
 // ---RENDERING---//
@@ -170,10 +170,18 @@ var projection;
 // time slider
 var playing = false;
 var processID;
-var timeSlider;
-var sliderStartValue;
-// var interval;
-// var startDate;
+
+
+var currentSliderValue;
+
+
+//var timeSlider;
+//var sliderStartValue;
+//var sliderEndValue;
+
+var interval = 86400000;
+var tick = 1;
+
 
 d3.json("data/ebov_discrete.json", function ready(error, json) {
 
@@ -185,42 +193,20 @@ d3.json("data/ebov_discrete.json", function ready(error, json) {
 	var startDate = new Date(timeLine.startTime);
 	var endDate = new Date(timeLine.endTime);
 
-	sliderStartValue = Date.parse(timeLine.startTime);
-
+	var sliderStartValue = Date.parse(timeLine.startTime);
+	var sliderEndValue = Date.parse(timeLine.endTime);
+	currentSliderValue = sliderStartValue;
+	
+	
 	// initial value
 	var currentDateDisplay = d3.select('#currentDate').text(
 			dateFormat(startDate));
 
 	var timeScale = d3.time.scale.utc().domain([ startDate, endDate ]).range(
 			[ 0, 1 ]);
-	timeSlider = d3.slider().scale(timeScale).axis(d3.svg.axis());
+	var timeSlider = d3.slider().scale(timeScale).axis(d3.svg.axis());
 	d3.select('#timeSlider').call(timeSlider);
 
-	// TODO: play/pause button for timeSlider
-	var playPauseButton = d3.select('#playPause').attr("class", "playPause")
-			.on("click", function() {
-
-				// sliderValue = sliderValue + 1;// interval;
-				// timeSlider.value(sliderValue);
-				// timeSlider.value(1386612676112);
-
-				if (playing) {
-					playing = false;
-					playPauseButton.classed("playing", playing);
-
-					clearInterval(processID);
-
-				} else {
-					playing = true;
-					playPauseButton.classed("playing", playing);
-
-					processID = setInterval(function() {
-						testTimer();
-					}, 100);
-
-				}// END: playing check
-
-			});
 
 	// ---ATTRIBUTES---//
 
@@ -252,7 +238,7 @@ d3.json("data/ebov_discrete.json", function ready(error, json) {
 
 		function readynow(error, world) {
 
-			generateWorldLayer(world)
+			generateWorldLayer(world);
 			// mapRendered = true;
 
 			// ---DATA LAYERS---//
@@ -262,18 +248,63 @@ d3.json("data/ebov_discrete.json", function ready(error, json) {
 			initializeLayers(layers);
 
 		}
-		;
 
 		queue().defer(d3.json, "data/world.geojson").await(readynow);
 
 	} else {
 
-		// ---DATA LAYERS---//
-
+		// ---TIME SLIDER---//
+		
 		initializeTimeSlider(timeSlider, timeScale, currentDateDisplay,
 				dateFormat);
+		
+		var playPauseButton = d3.select('#playPause').attr("class", "playPause")
+		.on("click", function() {
+
+//			timeSlider.value(1386612676112);
+			
+			if (playing) {
+				playing = false;
+				playPauseButton.classed("playing", playing);
+
+				clearInterval(processID);
+
+			} else {
+				playing = true;
+				playPauseButton.classed("playing", playing);
+
+				processID = setInterval(function() {
+
+					var sliderValue = currentSliderValue + tick * interval;
+					if (sliderValue > sliderEndValue) {
+						tick = 1;
+					}
+
+					timeSlider.value(sliderValue);
+					paintFrame(sliderValue, timeScale, currentDateDisplay,
+							dateFormat);
+					
+					currentSliderValue = sliderValue;
+					tick++;
+					
+				}, 100);
+
+			}// END: playing check
+
+		});		
+		
+		
+		// ---DATA LAYERS---//
+
 		initializeLayers(layers);
 
+		
+		///---EXPERIMENTAL---//
+		
+		///---END: EXPERIMENTAL---//	
+		
+		
+		
 	}// END: mapRendered check
 
 } // END: function
