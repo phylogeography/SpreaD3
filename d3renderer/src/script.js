2d410dfdd67aef53af300c5fa6832d0e899d9e97
fbielejec <fbielejec@gmail.com>
2015-10-13 12:45:50 +0200

    slider for line bend

--- d3renderer/src/script.js
+++ d3renderer/src/script.js
@@ -373,8 +373,8 @@ var sliderStartValue;
 var sliderEndValue;
 
 // d3.json("data/ebov_discrete.json", function ready(error, json) {
-// d3.json("data/continuous_test.json", function ready(error, json) {
-d3.json("data/antigenic_test.json", function ready(error, json) {
+ d3.json("data/continuous_test.json", function ready(error, json) {
+//d3.json("data/antigenic_test.json", function ready(error, json) {
 
 	// -- TIME LINE-- //
 
